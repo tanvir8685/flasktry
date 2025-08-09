5e3a110cefe49ddc574b50d9cfb554e3f1c7a9cf
Tanvirul Islam <tanvirhridoy1011@gmail.com>
2025-08-09 22:46:45 +0600

    basic login part

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
 from flask import Flask ,request, redirect, url_for,session,Response
+
+app=Flask(__name__)
+
+@app.route("/",method=["GET","POST"])
+def login():
+    if request.method=="POST":
+        username=request.form.get("username")
+        password=request.form.get("password")
+
+        if username=="admin" and password="123":
+            session["user"] = username
+            return redirect(url_for("welcome"))
+        else:
+            return Response ("In valid credentials Try again",mimetype="text/plain")
